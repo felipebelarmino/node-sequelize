5258727c4836f3cf2019e4faf8a15b20870aff61
felipebelarmino <epilefgomes1@gmail.com>
2021-04-08 12:41:15 -0300

    Corrigido bug na porta

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ app.get('/', (req, res) => {
   res.json({message: 'Say hello to my little friend'})
 });
 
-
 const PORT = process.env.PORT || 8080;
-
-// const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`Server funcionando na porta ${PORT}`);
