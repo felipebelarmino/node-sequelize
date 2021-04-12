39cd5a4ea2b0cce9d1991872a4ed9a6dc267c29c
felipebelarmino <epilefgomes1@gmail.com>
2021-04-12 19:17:51 -0300

    Alterado rota login

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -164,16 +164,7 @@ exports.findByLogin = (req, res) => {
 
   const allUsers = admin.findAll({
     where: null,
-  })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving admins.",
-      });
-    });
+  });
 
   const { Login, Password } = req.body;
 
